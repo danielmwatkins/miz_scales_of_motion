ef3fd9dc5b4346d1e61d0c7aed763515123a63d9
daniel watkins <danielmwatkins@gmail.com>
2024-10-16 16:09:53 -0400

    Updated fig1

--- scripts/03_plot_data_summary.py
+++ scripts/03_plot_data_summary.py
@@ -1,27 +1,164 @@
-import proplot as pplt
-import pandas as pd
+import cartopy.crs as ccrs
 import numpy as np
 import os
+import proplot as pplt
+import pandas as pd
+import rasterio as rio
+from rasterio.plot import reshape_as_image
+import skimage
+import warnings
 
+warnings.simplefilter('ignore')
+pplt.rc['cartopy.circular'] = False
+pplt.rc['reso'] = 'med'
+
+
+# Data ingest: shape properties and trajectories
 floe_lib_raw = {}
 for file in os.listdir('../data/floe_tracker/ift_floe_property_tables/raw/'):
-    if 'csv' in file:
+    if 'csv' in file: 
         year = int(file.replace('.csv', '').split('_')[-1])
         floe_lib_raw[year] = pd.read_csv('../data/floe_tracker/ift_floe_property_tables/raw/' + file,
                                          index_col=0, dtype={'classification': str})
         floe_lib_raw[year]['datetime'] = pd.to_datetime(floe_lib_raw[year]['datetime'])
 floe_lib_raw = pd.concat(floe_lib_raw)
-floe_lib_clean = pd.read_csv('../data/floe_tracker/ift_floe_properties.csv', index_col=0)
-floe_lib_clean['datetime'] = pd.to_datetime(floe_lib_clean['datetime'])
+
+floe_lib_clean = {}
+for file in os.listdir('../data/floe_tracker/ift_floe_property_tables/clean/'):
+    if 'csv' in file: 
+        year = int(file.replace('.csv', '').split('_')[-1])
+        floe_lib_clean[year] = pd.read_csv('../data/floe_tracker/ift_floe_property_tables/clean/' + file,
+                                         index_col=0, dtype={'classification': str})
+        floe_lib_clean[year]['datetime'] = pd.to_datetime(floe_lib_clean[year]['datetime'])
+floe_lib_clean = pd.concat(floe_lib_clean)
 
 trajectories = pd.read_csv('../data/floe_tracker/ift_floe_trajectories.csv', index_col=0)
 trajectories['datetime'] = pd.to_datetime(trajectories['datetime'])
 
-import cartopy.crs as ccrs
-import warnings
-warnings.simplefilter('ignore')
-pplt.rc['cartopy.circular'] = False
-pplt.rc['reso'] = 'med'
+
+ift_images = {}
+ift_clean = {}
+man_images = {}
+dataframes = {}
+tc_images = {}
+
+df = pd.read_csv('../data/floe_tracker/ift_floe_properties.csv', index_col=0)
+df['datetime'] = pd.to_datetime(df['datetime'].values)
+
+for date in ['2013-04-24 12:39:09', '2014-05-01 12:14:19']:
+    date = pd.to_datetime(date)
+    ift_images[date] = skimage.io.imread('../data/modis_imagery/{d}.aqua.labeled_raw.250m.tiff'.format(d=date.strftime('%Y%m%d')))
+    ift_clean[date] = skimage.io.imread('../data/modis_imagery/{d}.aqua.labeled_clean.250m.tiff'.format(d=date.strftime('%Y%m%d')))
+    tc_images[date] = rio.open('../data/modis_imagery/{d}.aqua.truecolor.250m.tiff'.format(d=date.strftime('%Y%m%d'))).read()
+
+    im_manual = skimage.io.imread('../data/validation_images/{d}.aqua.labeled_manual.png'.format(d=date.strftime('%Y%m%d')))
+    man_images[date] = skimage.measure.label(im_manual[:,:,0])
+    dataframes[date] = df.loc[df.datetime == date].reset_index(drop=True)
+
+    dataframes[date]['manual_label'] = np.nan
+    dataframes[date]['manual_area'] = np.nan
+    
+    for region in skimage.measure.regionprops(man_images[date]):
+        label = np.unique(ift_images[date][(ift_images[date] > 0) & (man_images[date] == region.label)])
+        if len(label) == 1:
+            dataframes[date].loc[dataframes[date].label == label[0], 'manual_label'] = region.label
+            dataframes[date].loc[dataframes[date].label == label[0], 'manual_area'] = region.area
+        
+
+
+############ Figure 1: Shape detection and area adjustment ################
+fig, axs = pplt.subplots(ncols=3, nrows=2, share=False, sharex=False, spany=False)
+
+imdate = pd.to_datetime('2014-05-01 12:14:19')
+overlap_floes = floe_lib_clean.loc[(floe_lib_clean.datetime.dt.year == 2014) & \
+    (floe_lib_clean.floe_id != 'unmatched')].groupby('floe_id').filter(
+        lambda x: imdate in x.datetime.values)
+
+date = '20140501'
+tc_image = rio.open('../data/modis_imagery/{d}.aqua.truecolor.250m.tiff'.format(d=date))
+fc_image = rio.open('../data/modis_imagery/{d}.aqua.falsecolor.250m.tiff'.format(d=date))
+pp_image = rio.open('../data/modis_imagery/{d}.aqua.preprocessed.250m.tiff'.format(d=date)).read().squeeze()
+
+left, bottom, right, top = tc_image.bounds
+left /= 1e6
+bottom /= 1e6
+right /= 1e6
+top /= 1e6
+
+for ax, image in zip([axs[0, 0], axs[0, 1]], [tc_image, fc_image]):
+    ax.imshow(reshape_as_image(image.read()), extent=[left, right, bottom, top])
+    
+    
+ax = axs[0, 2]
+ax.imshow(pp_image, cmap='mono_r', extent=[left, right, bottom, top])
+
+lb_raw_image = rio.open('../data/modis_imagery/{d}.aqua.labeled_raw.250m.tiff'.format(d=date)).read()
+lb_clean_image = rio.open('../data/modis_imagery/{d}.aqua.labeled_clean.250m.tiff'.format(d=date)).read()
+ax.imshow(np.ma.masked_array(reshape_as_image(lb_raw_image),
+                                reshape_as_image(lb_raw_image)==0), color='r', alpha=0.75, extent=[left, right, bottom, top])
+ax.imshow(np.ma.masked_array(reshape_as_image(lb_clean_image),
+                                reshape_as_image(lb_clean_image)==0), color='gold', alpha=1, extent=[left, right, bottom, top])
+
+h = [ax.plot([],[], color=c, alpha=a, lw=0, marker='s') for c, a in zip(['r', 'gold', 'k'], [0.5, 1, 1])]
+
+ax.legend(h, ['Non-floes', 'Floes', 'Masked'], loc='ll', ncols=1, alpha=1)  
+for ax in axs:
+    ax.format(ylim=(-2, -1.5), xlim=(0.6, 1.1))
+axs.format(abc=True)
+for ax, title in zip(axs[0,:], ['True Color', 'False Color', 'Processed']):
+    ax.format(title=title, ylabel='Y (m $\\times 10^6$)', xlabel='X (m $\\times 10^6)$')
+
+
+
+for ax, date in zip(axs[1,:], man_images):
+    ax.imshow(reshape_as_image(tc_images[date]), extent=[left, right, bottom, top])
+    outlines = man_images[date] - skimage.morphology.erosion(man_images[date], skimage.morphology.disk(4))
+    # ax.imshow(np.ma.masked_array(man_images[date], man_images[date]==0), c='k')
+    ax.imshow(np.ma.masked_array(ift_images[date], ift_images[date]==0), c='r', extent=[left, right, bottom, top], alpha=0.75)
+    ax.imshow(np.ma.masked_array(ift_clean[date], ift_clean[date]==0), c='gold', extent=[left, right, bottom, top], alpha=1)
+    ax.pcolorfast(np.linspace(left, right, outlines.shape[1]),
+                  np.linspace(top, bottom, outlines.shape[0]),
+                  np.ma.masked_array(outlines, mask=outlines == 0), color='b')
+    ax.format(xlim=(0.75, 0.95), ylim=(-1.6, -1.4), xtickminor=False, ytickminor=False, xlocator=0.1, ylocator=0.1,
+              title=date.strftime('%Y-%m-%d'), yreverse=False)
+
+h = [ax.plot([],[], ls=ls, m=m, lw=lw, color=c) for lw, c, ls, m in zip([1, 3, 3], ['k', 'gold', 'r'], ['-', '', ''], ['', 's', 's'])]
+axs[1, 0].legend(h, ['Manual', 'IFT floes', 'IFT non-floes'], loc='ul', ncols=1, alpha=1)
+
+for ax in axs[1, 0:2]:
+    ax.format(ylabel='Y (m $\\times 10^6$)', xlabel='X (m $\\times 10^6)$')
+
+
+for date in dataframes:
+    df_date = dataframes[date]
+    A_ift = df_date.loc[df_date.manual_area.notnull(), 'area'].values
+    A_man = df_date.loc[df_date.manual_area.notnull(), 'manual_area'].values
+    A_adj = (A_ift**0.5 + 6)**2
+    
+    # convert to km2
+    A_man *= 0.25**2
+    A_adj *= 0.25**2
+    A_ift *= 0.25**2
+    
+    axs[1, 2].scatter(A_man, A_ift, marker='.', color='gold')
+    axs[1, 2].scatter(A_man, A_adj, marker='+', color='b')
+axs[1, 2].format(ylim=(10, max(A_man)), xlim=(10, max(A_man)),
+                 yscale='log', xscale='log', xlabel='Manual', ylabel='Automatic', title='Area Adjustment')
+axs[1, 2].plot([0, max(A_man)], [0, max(A_man)], color='k', ls='--')
+
+h = [axs[1, 2].plot([],[],marker=m, color=c, lw=lw, ls=ls, alpha=a)
+             for a, m, c, lw, ls in zip([0.5, 1, 1], ['.', '+', ''], ['gold', 'b', 'k'], [0,0,1], ['', '', '--'])]
+axs[1, 2].legend(h, ['Raw', 'Adjusted', '1:1'], ncols=1, loc='ul')
+
+fig.save('../figures/fig02_algorithm_example.pdf', dpi=300)
+fig.save('../figures/fig02_algorithm_example.png', dpi=300)
+
+
+######## Figure 2: Trajectory example and rotation rates #########
+
+
+
+######## Figure 3: Summary of data availability
 
 year = 2004
 xmin = 0.2e6
@@ -69,47 +206,9 @@ axs[2].format(title='Long trajectories')
 axs.format(abc=True)
 fig.save('../figures/fig01_spatial_histogram.pdf', dpi=300)
 
-import rasterio as rio
-from rasterio.plot import reshape_as_image
-imdate = pd.to_datetime('2014-05-01 12:14:19')
-overlap_floes = floe_lib_clean.loc[(floe_lib_clean.datetime.dt.year == 2014) & \
-    (floe_lib_clean.floe_id != 'unmatched')].groupby('floe_id').filter(
-        lambda x: imdate in x.datetime.values)
 
-fig, axs = pplt.subplots(ncols=3, sharey=True, nrows=1, width=8, sharex=False, spany=False)
-for row, date in zip([0, 1], ['20140501']):
-    tc_image = rio.open('../data/modis_imagery/{d}.aqua.truecolor.250m.tiff'.format(d=date))
-    fc_image = rio.open('../data/modis_imagery/{d}.aqua.falsecolor.250m.tiff'.format(d=date))
-    pp_image = rio.open('../data/modis_imagery/{d}.aqua.preprocessed.250m.tiff'.format(d=date)).read().squeeze()
-    
-    left, bottom, right, top = tc_image.bounds
-    left /= 1e6
-    bottom /= 1e6
-    right /= 1e6
-    top /= 1e6
-    
-    for ax, image in zip([axs[0], axs[1]], [tc_image, fc_image]):
-        ax.imshow(reshape_as_image(image.read()), extent=[left, right, bottom, top])
-        
-    
-    ax = axs[2]
-    ax.imshow(pp_image, cmap='mono_r', extent=[left, right, bottom, top])
-    
-    lb_raw_image = rio.open('../data/modis_imagery/{d}.aqua.labeled_raw.250m.tiff'.format(d=date)).read()
-    lb_clean_image = rio.open('../data/modis_imagery/{d}.aqua.labeled_clean.250m.tiff'.format(d=date)).read()
-    ax.imshow(np.ma.masked_array(reshape_as_image(lb_raw_image),
-                                    reshape_as_image(lb_raw_image)==0), color='r', alpha=0.5, extent=[left, right, bottom, top])
-    ax.imshow(np.ma.masked_array(reshape_as_image(lb_clean_image),
-                                    reshape_as_image(lb_clean_image)==0), color='b', alpha=1, extent=[left, right, bottom, top])
 
-    h = [ax.plot([],[], color=c, alpha=a, lw=0, marker='s') for c, a in zip(['r', 'b', 'k'], [0.5, 1, 1])]
 
-    ax.legend(h, ['Non-floes', 'Floes', 'Masked'], loc='ll', ncols=1)  
-
-axs.format(abc=True)
-for ax, title in zip(axs, ['True Color', 'False Color', 'Processed']):
-    ax.format(title=title, ylabel='Y (m $\\times 10^6$)', xlabel='X (m $\\times 10^6)$')
-fig.save('../figures/fig02_image_processing_1x3.pdf', dpi=300)
 
 data = []
 for year in range(2003, 2021):
